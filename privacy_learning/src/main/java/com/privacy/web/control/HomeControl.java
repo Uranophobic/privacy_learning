a203223fea5b67888c49433fd0b1a9f0c7abdbfa
Aless <Aless@192.168.0.120>
2023-03-30 13:10:21 +0200

    Insert homepage control, homepage html, navbar html, stylesheet (bozze)

---
+++ privacy_learning/src/main/java/com/privacy/web/control/HomeControl.java
@@ -0,0 +1,14 @@
+package com.privacy.web.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller		
+public class HomeControl {
+
+	@GetMapping("/homepage")
+	public String home(Model model) {
+		return "HomePage";
+	}
+}
