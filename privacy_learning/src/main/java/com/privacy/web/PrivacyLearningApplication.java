db41f2994de26678242694da657e1a34ab4e7cb3
Aless <Aless@192.168.0.120>
2023-03-30 12:41:15 +0200

    first commit

---
+++ privacy_learning/src/main/java/com/privacy/web/PrivacyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.privacy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacyLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacyLearningApplication.class, args);
+	}
+
+}
---
+++ privacy_learning/src/test/java/com/privacy/web/PrivacyLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.privacy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivacyLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
