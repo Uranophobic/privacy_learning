babd8f6e0c02c71e5a66c9c0d33720b70551a004
Alessia <Alessia@DESKTOP-5P90JI1.station>
2023-04-03 18:25:17 +0200

    fix db

--- privacy_learning/src/main/java/com/privacy/web/PrivacyLearningApplication.java
+++ privacy_learning/src/main/java/com/privacy/web/PrivacyLearningApplication.java
@@ -1,8 +1,14 @@
 package com.privacy.web;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.privacy.web.repository")
 public class PrivacyLearningApplication {
 
 	public static void main(String[] args) {
